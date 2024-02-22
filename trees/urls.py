a27c0e82e0c2b3fdc89c0e4e1e083c48b73133e2
Rayanne <mouraribeiro11@gmail.com>
2024-02-21 23:18:00 -0300

    url do json

--- trees/urls.py
+++ trees/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', SingUp.as_view(), name='singup'),
     path('', Plant_list, name='plantlist'),
     path('plant/<int:id>/', PlantView, name='plant list'),
-    path('create/', plant_tree, name='create plant'),
+    path('create/', plant_tree, name='create-plant'),
     path('json/', plants_json, name='json plant'),
 
 ]

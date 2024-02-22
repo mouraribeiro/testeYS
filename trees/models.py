bad790d055d4600c44f34d7c53a0a37dfcc811fa
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-02-22 12:04:07 -0300

    ajustes e retorno metodo json api

--- trees/models.py
+++ trees/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     about = models.TextField(max_length=500)
-    joined = models.DateTimeField()
+    joined = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Profile(models.Model):
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField()
     user = models.ManyToManyField(User)
 
@@ -31,13 +31,17 @@ class Tree(models.Model):
         return self.name
 
 
+# aqui seria melhor geofield
 class Location(models.Model):
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
 
+    def __str__(self):
+        return str(self.lon) + ' , ' + str(self.lat)
+
 class Plant(models.Model):
     age = models.IntegerField()
-    planted_at = models.DateTimeField()
+    planted_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tree = models.ForeignKey(Tree, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE , null=True)
